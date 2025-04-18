9aab7e92ad0e871dbde16f982f6c2de03e23af1b
Rigelyon <pandudwi120@gmail.com>
2025-04-18 14:54:59 +0700

    Remove redundant import statements in URL configuration

--- PixAni/urls.py
+++ PixAni/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +24,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    from django.conf import settings
     from django.conf.urls.static import static
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
